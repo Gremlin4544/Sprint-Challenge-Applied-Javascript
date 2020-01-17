9da4924d5111bf1c5da03b90fcd75e93b9b50c2d
Gremlin4544 <alve@mckethen.com>
2020-01-17 11:46:16 -0500

    updated readme and completed header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -26,8 +26,30 @@ function Header(item) {
 
     //Define new elements
 
-    const header = document.createElement('div');,
-          date = document.createElement('span');,
-          title = document.createElement('h1');,
-          temp = document.createElement('span');  
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    //Set class names
+
+    header.classList.add('head');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    //Set text content
+
+    date.textContent = item.date;
+    title.textContent = item.title;
+    temp.textContent = item.temp;
+    
+
+    //Setup structure of elements
+
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    return header;
 }
+
